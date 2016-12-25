be6b2a3be05e8dbd1d9fa7d99ea151aa4e262531
zhenghaijue <zhenghaijue@foxmail.com>
2016-12-25 16:15:08 +0800

    实现继承的两种方式

--- blog/object.js
+++ blog/object.js
@@ -1,23 +1,23 @@
 //利用Object.create()实现继承
-// function Shape(x,y){
-// 	this.x = x;
-// 	this.y = y;
-// }
-// Shape.prototype.move = function(x,y){
-// 	this.x += x;
-// 	this.y += y;
-// }
-// function Rectangle(x,y,z,p){
-// 	Shape.call(this,x,y) //    => this.tempMethod=Shape;this.tempMethod(x,y);   
-// 	this.z = z;
-// 	this.p = p;
-// }
-// Rectangle.prototype = Object.create(Shape.prototype)
-// var rect = new Rectangle(1,2,3,4)
-// console.log(rect instanceof Rectangle)
-// console.log(rect instanceof Shape)
-// rect.move(1,2)
-// console.log(rect.x)
+function Shape(x,y){
+	this.x = x;
+	this.y = y;
+}
+Shape.prototype.move = function(x,y){
+	this.x += x;
+	this.y += y;
+}
+function Rectangle(x,y,z,p){
+	Shape.call(this,x,y) //    => this.tempMethod=Shape;this.tempMethod(x,y);   
+	this.z = z;
+	this.p = p;
+}
+Rectangle.prototype = Object.create(Shape.prototype)
+var rect = new Rectangle(1,2,3,4)
+console.log(rect instanceof Rectangle)
+console.log(rect instanceof Shape)
+rect.move(1,2)
+console.log(rect.x)
 //利用ES 6 class关键字实现继承
 class Person{
 	constructor(name,age){

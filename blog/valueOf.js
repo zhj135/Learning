b62d462ce952046a682190cbdb2b46504ac43cb4
zhenghaijue <zhenghaijue@foxmail.com>
2017-01-18 11:22:16 +0800

    重写valueOf()方法

--- blog/valueOf.js
+++ blog/valueOf.js
@@ -1,3 +1,13 @@
 //写一个mul函数调用时将生成以下输出:
 // console.log(mul(2)(3)(4)); // output : 24
 // console.log(mul(4)(3)(4)); // output : 48
+function mul(x){
+	var result = function(y){
+		return mul(x*y);
+	}
+	result.valueOf = function(){
+		return x;
+	}
+	return result
+}
+console.log(mul(2)(3)(4)(5))

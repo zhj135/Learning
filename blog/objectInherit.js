6be51f66a07cd5c3fba272b029d33a61f7088269
zhj <zhenghaijue@foxmail.com>
2017-02-21 18:09:08 +0800

    添加一种继承方式

--- blog/objectInherit.js
+++ blog/objectInherit.js
@@ -39,3 +39,24 @@ class Teacher extends Person{
 }
 var obj = new Teacher('jue',23,'Math')
 obj.getSubject();
+
+//React中ReactPureComponent继承ReactComponent的方式
+function People(name,age){
+	this.name = name;
+	this.age = age;
+}
+People.prototype.sayName = function(){
+	console.log('my name is ' + this.name)
+}
+function Teacher(name,age,subject){
+	this.name = name;
+	this.age = age;
+	this.subject = subject;
+}
+var ComponentDummy = function(){};
+ComponentDummy.prototype = People.prototype;
+Teacher.prototype = new ComponentDummy();
+Teacher.prototype.constructor = Teacher;
+var t1 = new Teacher('wnag',40,'math');
+t1.sayName();
+console.log(t1.constructor)

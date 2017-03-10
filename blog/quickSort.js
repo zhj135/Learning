78a82eeb203942d079c614e751c63f661a285953
zhj <zhenghaijue@foxmail.com>
2017-03-10 19:11:51 +0800

    123

--- blog/quickSort.js
+++ blog/quickSort.js
@@ -16,18 +16,32 @@ console.log(arr);
 // 		}
 // 	}
 // })
+function swap(arr, a, b) {
+	let temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+}
 function quickSort(arr){
+	if (arr.length <= 1) {
+		return arr;
+	}
 	let mid = arr[0];
-	let finalIndex = 0;
+	let finalIndex = 1;
+	let left = [];
+	let right = [];
 	for(let i=1;i<arr.length;i++){
-		if (arr[i] < mid) {
+		if (arr[i] <= mid) {
+			swap(arr, i, finalIndex)
 			finalIndex++;
 		}
-		console.log(finalIndex)
 	}
-	let temp = mid;
-	mid = arr[finalIndex];
-	arr[finalIndex] = temp;
+	swap(arr, 0, finalIndex-1)
 	console.log(arr)
+	left = arr.slice(0, finalIndex-1);
+	console.log('left: ' + left)
+	right = arr.slice(finalIndex, arr.length)
+	console.log('right: ' + right)
+	return quickSort(left).concat(arr[finalIndex-1], quickSort(right))
 }
 quickSort(arr);
+console.log(arr)

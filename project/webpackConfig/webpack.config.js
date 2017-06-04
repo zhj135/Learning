de495f16383ccc77571d8c20585fd5f138721c65
zhj <zhenghaijue@foxmail.com>
2017-06-04 22:07:38 +0800

    next step: NODE_ENV

--- project/webpackConfig/webpack.config.js
+++ project/webpackConfig/webpack.config.js
@@ -2,68 +2,60 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+console.log('NODE_ENV', process.env.NODE_ENV);
 module.exports = {
     context: resolve(__dirname, 'src'),
-
     entry: [
         'react-hot-loader/patch',
         // activate HMR for React
-
         'webpack-dev-server/client?http://localhost:8080',
         // bundle the client for webpack-dev-server
         // and connect to the provided endpoint
-
         'webpack/hot/only-dev-server',
         // bundle the client for hot reloading
         // only- means to only hot reload for successful updates
-
         './index.js'
         // the entry point of our app
     ],
     output: {
-        filename: 'bundle.js',
+        filename: 'app.js',
         // the output bundle
-
         path: resolve(__dirname, 'dist'),
-
         publicPath: '/'
             // necessary for HMR to know where to load the hot update chunks
     },
-
     devtool: 'inline-source-map',
-
     devServer: {
         hot: true,
         // enable HMR on the server
-
-        contentBase: resolve(__dirname, 'dist'),
-        // match the output path
-
+        contentBase: resolve(__dirname),
+        // 开发模式中index.html位置
         publicPath: '/'
             // match the output `publicPath`
     },
-
     module: {
         rules: [{
             test: /\.jsx?$/,
-            use: ['babel-loader', ],
+            use: ['babel-loader'],
             exclude: /node_modules/
         }, {
             test: /\.css$/,
             // use: ['style-loader', 'css-loader?modules' ],
             use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: "css-loader"
+                fallback: 'style-loader',
+                use: 'css-loader'
             })
-        }, ],
+        }],
     },
-
     plugins: [
-        new ExtractTextPlugin("styles.css"),
+        new ExtractTextPlugin('app.css'),
         new webpack.HotModuleReplacementPlugin(),
         // enable HMR globally
-
         new webpack.NamedModulesPlugin(),
         // prints more readable module names in the browser console on HMR updates
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': 'dev'
+        })
+
     ],
 };
